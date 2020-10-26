0b18b10a1f15864bc509cdd9ee4bdc3fd22f60d9
Alex Nault <nault.alex@gmail.com>
2020-10-26 19:35:22 -0400

    Add lighthouserc.js

---
+++ lighthouserc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  ci: {
+    upload: {
+      target: "temporary-public-storage",
+    },
+    // assert: {
+    //   preset: "lighthouse:recommended",
+    //   assertions: {
+    //     "maskable-icon": "off",
+    //     "uses-rel-preconnect": "off",
+    //     "works-offline": "off",
+    //     "unused-javascript": "off",
+    //     "unused-css-rule": "off",
+    //     "offline-start-url": "off",
+    //     "service-worker": "off",
+    //   },
+    // },
+  },
+};
