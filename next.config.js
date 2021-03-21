89300a5e9fcecdb0d17356ec099c99129872b4a4
Alex Nault <nault.alex@gmail.com>
2021-03-21 00:35:05 -0400

    Add security headers

--- next.config.js
+++ next.config.js
@@ -1,7 +1,38 @@
 const withPWA = require("next-pwa");
+const { createSecureHeaders } = require("next-secure-headers");
 
 module.exports = withPWA({
   pwa: {
     dest: "public",
+  },
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: createSecureHeaders({
+          contentSecurityPolicy: {
+            directives: {
+              defaultSrc: [
+                "'self'",
+                "https://alexnault.dev",
+                "https://excalidraw.com",
+                "https://www.googletagmanager.com",
+                "https://www.google-analytics.com",
+              ],
+              styleSrc: ["'self'", "'unsafe-inline'"],
+              scriptSrc: [
+                "'self'",
+                "'unsafe-inline'",
+                "https://www.googletagmanager.com",
+              ],
+              imgSrc: ["'self'"],
+              baseUri: "self",
+              formAction: "self",
+              frameAncestors: true,
+            },
+          },
+        }),
+      },
+    ];
   },
 });
