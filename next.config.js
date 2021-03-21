928abcc1831f4c0b3c86e7109bdc1cb9e017e20c
Alex Nault <nault.alex@gmail.com>
2021-03-21 00:51:22 -0400

    Add security headers (#13)
    * Add security headers
    
    * Add Vercel vitals
    
    * Add data image

--- next.config.js
+++ next.config.js
@@ -1,7 +1,39 @@
 const withPWA = require("next-pwa");
+const { createSecureHeaders } = require("next-secure-headers");
 
 module.exports = withPWA({
   pwa: {
     dest: "public",
+  },
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: createSecureHeaders({
+          contentSecurityPolicy: {
+            directives: {
+              defaultSrc: [
+                "'self'",
+                "https://alexnault.dev",
+                "https://excalidraw.com",
+                "https://www.googletagmanager.com",
+                "https://www.google-analytics.com",
+                "https://vitals.vercel-insights.com",
+              ],
+              styleSrc: ["'self'", "'unsafe-inline'"],
+              scriptSrc: [
+                "'self'",
+                "'unsafe-inline'",
+                "https://www.googletagmanager.com",
+              ],
+              imgSrc: ["'self'", "data:"],
+              baseUri: "self",
+              formAction: "self",
+              frameAncestors: true,
+            },
+          },
+        }),
+      },
+    ];
   },
 });
