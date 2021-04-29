7292f91e2bf1aca16b0a10f4d7708905ac858170
Alex Nault <nault.alex@gmail.com>
2021-04-28 21:05:14 -0400

    Fix prop and font-size

--- components/footer.tsx
+++ components/footer.tsx
@@ -4,15 +4,21 @@ import Divider from "components/divider";
 
 const Footer = () => (
   <footer className="flex justify-center py-10 px-4">
-    <a href="https://github.com/alexnault" className="text-gray-500">
+    <a href="https://github.com/alexnault" className="text-gray-500 sm:text-lg">
       GitHub
     </a>
-    <Divider orientation="vertical" className="h-auto mx-4" />
-    <a href="https://www.linkedin.com/in/naultalex" className="text-gray-500">
+    <Divider className="h-auto mx-4" />
+    <a
+      href="https://www.linkedin.com/in/naultalex"
+      className="text-gray-500 sm:text-lg"
+    >
       LinkedIn
     </a>
-    <Divider orientation="vertical" className="h-auto mx-4" />
-    <a href="https://twitter.com/nault_alex" className="text-gray-500">
+    <Divider className="h-auto mx-4" />
+    <a
+      href="https://twitter.com/nault_alex"
+      className="text-gray-500 sm:text-lg"
+    >
       Twitter
     </a>
   </footer>
