ae9a1789b632a104477eee8380f77489ce613f55
Alex Nault <nault.alex@gmail.com>
2021-09-10 18:37:05 -0400

    Disallow object-src CSP

--- next.config.js
+++ next.config.js
@@ -38,6 +38,7 @@ const config = {
               imgSrc: ["'self'", "data:"],
               baseUri: ["'self'"],
               formAction: ["'self'"],
+              objectSrc: ["'none'"],
               frameAncestors: true,
             },
           },
