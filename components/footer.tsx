a99eced254152a9cad4c7998a88651412e64b62e
Alex Nault <nault.alex@gmail.com>
2021-10-14 22:14:02 -0400

    Add navigation in footer (#25)

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,34 +1,48 @@
 import React from "react";
+import Link from "next/link";
 
 import Divider from "components/divider";
 
 type FooterLinkProps = {
-  title: string;
+  children?: React.ReactNode;
   href?: string;
 };
 
-function FooterLink({ title, href }: FooterLinkProps) {
+function FooterLink({ children, href }: FooterLinkProps) {
   return (
-    <a
-      href={href}
-      className="transition-colors text-gray-500 sm:text-lg hover:text-black"
-    >
-      {title}
+    <a href={href} className="transition-colors text-gray-500 hover:text-black">
+      {children}
     </a>
   );
 }
 
 export default function Footer() {
   return (
-    <footer className="flex justify-center py-10 px-6">
-      <FooterLink title="GitHub" href="https://github.com/alexnault" />
-      <Divider className="h-auto mx-4" />
-      <FooterLink
-        title="LinkedIn"
-        href="https://www.linkedin.com/in/naultalex"
-      />
-      <Divider className="h-auto mx-4" />
-      <FooterLink title="Twitter" href="https://twitter.com/nault_alex" />
+    <footer className="py-10 px-6 space-y-6">
+      <nav className="flex justify-center">
+        <Link href="/" passHref>
+          <FooterLink>Home</FooterLink>
+        </Link>
+        <Divider className="h-auto mx-4" />
+        <Link href="/about" passHref>
+          <FooterLink>About</FooterLink>
+        </Link>
+        <Divider className="h-auto mx-4" />
+        <FooterLink href="mailto:nault.alex@gmail.com">Contact</FooterLink>
+        <Divider className="h-auto mx-4" />
+        <FooterLink href="https://github.com/alexnault/alexnault.dev">
+          Source
+        </FooterLink>
+      </nav>
+      <div className="flex justify-center ">
+        <FooterLink href="https://github.com/alexnault">GitHub</FooterLink>
+        <Divider className="h-auto mx-4" />
+        <FooterLink href="https://www.linkedin.com/in/naultalex">
+          LinkedIn
+        </FooterLink>
+        <Divider className="h-auto mx-4" />
+        <FooterLink href="https://twitter.com/nault_alex">Twitter</FooterLink>
+      </div>
     </footer>
   );
 }
