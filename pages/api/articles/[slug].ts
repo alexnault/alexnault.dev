fa5e83c7660b5061c8ebf78a0849592e4326ec3a
Alex Nault <nault.alex@gmail.com>
2022-06-29 20:58:57 -0400

    Use zod for runtime validation

--- pages/api/articles/[slug].ts
+++ pages/api/articles/[slug].ts
@@ -1,13 +1,24 @@
 import type { NextApiRequest, NextApiResponse } from "next";
+import { z } from "zod";
 
 import { likeRepo } from "repos/likes";
+
+const QuerySchema = z.object({
+  slug: z.string(),
+});
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
   try {
-    const slug = req.query.slug.toString();
+    const result = QuerySchema.safeParse(req.query);
+
+    if (!result.success) {
+      return res.status(400).json({ error: result.error });
+    }
+
+    const { slug } = result.data;
 
     if (req.method === "GET") {
       const like_count = await likeRepo.getArticleLikeCount(slug);
