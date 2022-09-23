f05fe2c55713ee32f71db78a037d333359b839a3
Alex Nault <nault.alex@gmail.com>
2022-09-23 17:04:06 -0400

    Fix eslint error

--- next.config.js
+++ next.config.js
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 const { withPlaiceholder } = require("@plaiceholder/next");
 const { createSecureHeaders } = require("next-secure-headers");
 
