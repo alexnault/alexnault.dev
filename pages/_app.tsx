676c7a6588231013fdc6af486d66e54ec4dbc2a2
Alex Nault <nault.alex@gmail.com>
2022-09-23 17:51:59 -0400

    Set staleTime and retry 0

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,30 @@
+import { useState } from "react";
 import Head from "next/head";
 import { AppProps } from "next/app";
 import { ThemeProvider } from "next-themes";
 import Script from "next/script";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import {
+  QueryClient,
+  QueryClientProvider,
+  Hydrate,
+} from "@tanstack/react-query";
 
 import "styles/fonts.css";
 import "styles/globals.css";
 
-const queryClient = new QueryClient();
-
 export default function App({ Component, pageProps }: AppProps) {
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            staleTime: 5000,
+            retry: 0,
+          },
+        },
+      })
+  );
+
   return (
     <ThemeProvider attribute="class">
       <Head>
@@ -43,7 +58,10 @@ export default function App({ Component, pageProps }: AppProps) {
         </>
       )}
       <QueryClientProvider client={queryClient}>
-        <Component {...pageProps} />
+        {/* @ts-expect-error */}
+        <Hydrate state={pageProps.dehydratedState}>
+          <Component {...pageProps} />
+        </Hydrate>
       </QueryClientProvider>
     </ThemeProvider>
   );
