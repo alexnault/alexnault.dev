0581c100db0aa9b24ec5d72bc927c1b311025a39
Alex Nault <nault.alex@gmail.com>
2023-03-06 19:56:05 -0500

    Add Permissions-Policy security header (#60)

--- next.config.js
+++ next.config.js
@@ -7,36 +7,42 @@ const config = {
   async headers() {
     return [
       {
-        source: "/(.*)",
-        headers: createSecureHeaders({
-          referrerPolicy: "origin-when-cross-origin",
-          contentSecurityPolicy: {
-            directives: {
-              defaultSrc: [
-                "'self'",
-                "alexnault.dev",
-                "excalidraw.com",
-                "*.googletagmanager.com",
-                "*.google-analytics.com",
-                "*.vercel-insights.com",
-              ],
-              styleSrc: ["'self'", "'unsafe-inline'"],
-              scriptSrc: [
-                "'self'",
-                "'unsafe-inline'",
-                "*.googletagmanager.com",
-                ...(process.env.NODE_ENV === "development"
-                  ? ["'unsafe-eval'"] // For hot reload
-                  : []),
-              ],
-              imgSrc: ["'self'", "data:"],
-              baseUri: ["'self'"],
-              formAction: ["'self'"],
-              objectSrc: ["'none'"],
-              frameAncestors: true,
+        source: "/:path*",
+        headers: [
+          ...createSecureHeaders({
+            referrerPolicy: "origin-when-cross-origin",
+            contentSecurityPolicy: {
+              directives: {
+                defaultSrc: [
+                  "'self'",
+                  "alexnault.dev",
+                  "excalidraw.com",
+                  "*.googletagmanager.com",
+                  "*.google-analytics.com",
+                  "*.vercel-insights.com",
+                ],
+                styleSrc: ["'self'", "'unsafe-inline'"],
+                scriptSrc: [
+                  "'self'",
+                  "'unsafe-inline'",
+                  "*.googletagmanager.com",
+                  ...(process.env.NODE_ENV === "development"
+                    ? ["'unsafe-eval'"] // For hot reload
+                    : []),
+                ],
+                imgSrc: ["'self'", "data:"],
+                baseUri: ["'self'"],
+                formAction: ["'self'"],
+                objectSrc: ["'none'"],
+                frameAncestors: true,
+              },
             },
+          }),
+          {
+            key: "Permissions-Policy",
+            value: "camera=(), microphone=(), geolocation=()",
           },
-        }),
+        ],
       },
     ];
   },
